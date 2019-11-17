fa92ee382a5a4419eda880c164bd1779864546b4
David RJ <david.rud135@gmail.com>
2019-11-17 19:35:54 +0200

    Added firebase config for production mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD6xl8T1P0N0Jmj6dZARNNe_auokmqTaUE',
+    authDomain: 'my-taskboard.firebaseapp.com',
+    databaseURL: 'https://my-taskboard.firebaseio.com',
+    projectId: 'my-taskboard',
+    storageBucket: 'my-taskboard.appspot.com',
+    messagingSenderId: '168592643320',
+    appId: '1:168592643320:web:6d0538a0aea56ab8ddd70d',
+    measurementId: 'G-SYTW18GEVQ',
+  },
 };
