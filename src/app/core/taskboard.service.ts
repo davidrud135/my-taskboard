7a4a3b28f3e31501d2a2bc76b5f2134396677991
David RJ <david.rud135@gmail.com>
2019-12-14 00:45:16 +0200

    Created method which simulates obtaining boards list from database.

--- src/app/core/taskboard.service.ts
+++ src/app/core/taskboard.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@angular/core';
+import { firestore } from 'firebase/app';
+
+import { Board } from './models/board.model';
+import { BoardBackColor } from './models/board-back-color.model';
 
 @Injectable({ providedIn: 'root' })
-export class TaskboardService {}
+export class TaskboardService {
+  getBoards(): Board[] {
+    const boards: Board[] = [];
+    Object.values(BoardBackColor).forEach((color: string, i: number) => {
+      boards.push({
+        id: `${i}`,
+        title: `Board #${i + 1}`,
+        adminId: `user${i}`,
+        membersIds: [],
+        backgroundColor: color,
+        createdAt: firestore.Timestamp.now(),
+      });
+    });
+    return boards;
+  }
+}
