60c033f00291ae334d9e5e29f2c3d787de6becb4
David RJ <david.rud135@gmail.com>
2019-12-14 00:11:41 +0200

    Created model for possible board background colors.

---
+++ src/app/core/models/board-back-color.model.ts
@@ -0,0 +1,13 @@
+/**
+ * Defines possible color scheme for board background color.
+ */
+export enum BoardBackColor {
+  Green = '#3cb44b',
+  Lime = '#bfef45',
+  Blue = '#4363d8',
+  Orange = '#f58231',
+  Pink = '#fabebe',
+  Brown = '#9A6324',
+  Red = '#e6194B',
+  Navy = '#000075',
+}
