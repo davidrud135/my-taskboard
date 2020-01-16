1174d26b1bf96d78f11e9dfd8cd1b9d9f8cd700d
David RJ <david.rud135@gmail.com>
2020-01-16 17:17:47 +0200

    Now Card has 'creatorId' property

--- src/app/core/models/firestore-card.model.ts
+++ src/app/core/models/firestore-card.model.ts
@@ -4,10 +4,12 @@ import { firestore } from 'firebase/app';
  * Defines model for Card data in Firestore.
  * @property {string} title - card title.
  * @property {string} description - card description.
+ * @property {string} creatorId - card creator id.
  * @property {firestore.Timestamp} createdAt - timestamp when card was created.
  */
 export interface FirestoreCard {
   title: string;
   description: string;
+  creatorId: string;
   createdAt: firestore.Timestamp;
 }
