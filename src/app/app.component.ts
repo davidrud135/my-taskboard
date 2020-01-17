de6702b5f7001593164a6a119201d06544b1040f
David RJ <david.rud135@gmail.com>
2020-01-17 16:30:45 +0200

    Header is not visible on auth pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  isAuthPage: boolean;
+
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        const { url } = event;
+        this.isAuthPage = url === '/sign-in' || url === '/sign-up';
+      }
+    });
+  }
+}
