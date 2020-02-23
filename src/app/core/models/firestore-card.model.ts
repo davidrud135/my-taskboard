9968ecb5bde4060d407a8faf2c82c12e0bab381a
David RJ <david.rud135@gmail.com>
2020-02-23 16:45:52 +0200

    Added 'tagsIds' property

--- src/app/core/models/firestore-card.model.ts
+++ src/app/core/models/firestore-card.model.ts
@@ -7,6 +7,7 @@ import { firestore } from 'firebase/app';
  * @property {string} description - card description.
  * @property {string} creatorId - card creator id.
  * @property {string[]} usersIdsWhoVoted - array of user's ids who have voted for the card.
+ * @property {string[]} tagsIds - array of card tag's ids.
  * @property {firestore.Timestamp} createdAt - timestamp when card was created.
  */
 export interface FirestoreCard {
@@ -14,5 +15,6 @@ export interface FirestoreCard {
   description: string;
   creatorId: string;
   usersIdsWhoVoted: any;
+  tagsIds: any;
   createdAt: firestore.Timestamp;
 }
