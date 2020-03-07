32bb2f4d84de8030fc8579fe84c53b43aa8f05c1
David RJ <david.rud135@gmail.com>
2020-03-08 01:45:43 +0200

    Added 'attachments' property to the Card

--- src/app/core/models/card.model.ts
+++ src/app/core/models/card.model.ts
@@ -1,6 +1,7 @@
 // tslint:disable: max-line-length
 import { firestore } from 'firebase/app';
 import { Tag } from './tag.model';
+import { CardAttachment } from './card-attachment.model';
 
 /**
  * Defines model for Card data.
@@ -10,6 +11,7 @@ import { Tag } from './tag.model';
  * @property {string} creatorId - card creator id.
  * @property {string[]} usersIdsWhoVoted - array of user's ids who have voted for the card.
  * @property {Tag[]} tagsIds - array of card's tags.
+ * @property {CardAttachment[]} attachments - array of card's attachments.
  * @property {firestore.Timestamp} createdAt - timestamp when card was created.
  */
 export interface Card {
@@ -19,5 +21,6 @@ export interface Card {
   creatorId: string;
   usersIdsWhoVoted: string[];
   tags: Tag[];
+  attachments: CardAttachment[];
   createdAt: firestore.Timestamp;
 }
